eabdd9bd88395619a8d18a97e362097def350a11
Giuliano Latini <latini.giuliano@gmail.com>
2018-04-15 22:54:45 +0200

    Add deploy and validation test for AZURE VM

--- MicrosoftAzure/Scripts/Test1.sh
+++ MicrosoftAzure/Scripts/Test1.sh
@@ -1,3 +1,25 @@
 az group create --name SandboxGroup --location "West Europe"
+# Deploy storage account
 az group deployment create --resource-group SandboxGroup --template-file ../Templates/AzureDeploy.json
+# Deploy VM
+az group deployment create \
+    --name RHEL-VOID-1 \
+    --resource-group SandboxGroup \
+    --template-file Templates/RHEL/azuredeploy.json \
+    --parameters @Templates/RHEL/azuredeploy.parameters.json
+
+# Validation Tests
+az group deployment show \
+    --name RHEL-VOID-1 \
+    --resource-group SandboxGroup 
+ssh-copy-id rheladmin@rhel-void-1.westeurope.cloudapp.azure.com
+ssh rheladmin@rhel-void-1.westeurope.cloudapp.azure.com
+# Check which packages maybe updates
+ssh rheladmin@rhel-void-1.westeurope.cloudapp.azure.com -t "sudo yum check-update"
+
+ansible all -i Ansible/inventories/staging/hosts -m ping
+
+
+# Cleaner
 az group delete --name SandboxGroup
+
