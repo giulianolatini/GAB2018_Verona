049ae8618b6a1d40106b465799d6991e4160e6cb
Giuliano Latini <latini.giuliano@gmail.com>
2018-04-20 09:42:46 +0200

    Add second WebApp example

--- MicrosoftAzure/Scripts/Test2.sh
+++ MicrosoftAzure/Scripts/Test2.sh
@@ -9,7 +9,7 @@ az webapp deployment user set \
 # Create Resource Group
 az group create \
     --name SandboxGroup02 \
-    --location "West Europe"
+    --location "North Europe"
 
 # Create AppPlan into Resource Group
 az appservice plan create \
@@ -21,6 +21,36 @@ az appservice plan create \
 # List runtime to use on linux
 az webapp list-runtimes
 az webapp list-runtimes --linux
+az appservice list-locations \
+    --sku S1 \
+    --linux-workers-enabled
+
+az sql server create \
+    --name DB-BOX \
+    --resource-group SandboxGroup02 \
+    --location "North Europe" \
+    --admin-user sandbox \
+    --admin-password SNBdxs210
+
+az sql server firewall-rule create \
+    --resource-group SandboxGroup02 \
+    --server db-sbox \
+    --name AllowAccessFromAzure \
+    --start-ip-address 0.0.0.0 \
+    --end-ip-address 0.0.0.0
+
+az sql db create \
+    --resource-group SandboxGroup02 \
+    --server db-sbox \
+    --name coreDB \
+    --service-objective S0
+
+Server=tcp:db-sbox.database.windows.net,1433;Initial Catalog=coreDB;Persist Security Info=False;User ID=sandbox;Password=SNBdxs210;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;
+
+# Create Distribution user
+az webapp deployment user set \
+    --user-name sandboxusr01 \
+    --password SNBdxs201
 
 # Create webapp
 az webapp create \
@@ -31,6 +61,19 @@ az webapp create \
     --deployment-local-git \
     | jq --raw-output '.deploymentLocalGitUrl, .defaultHostName'
 
+# Add Environment variables
+az webapp config connection-string set \
+    --resource-group SandboxGroup02 \
+    --name Webtest01 \
+    --settings MyDbConnection='Server=tcp:db-sbox.database.windows.net,1433;Initial Catalog=coreDB;Persist Security Info=False;User ID=sandbox;Password=SNBdxs210;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;' \
+    --connection-string-type SQLServer
+
+az webapp config appsettings set \
+    --name Webtest01 \
+    --resource-group SandboxGroup02 \
+    --settings ASPNETCORE_ENVIRONMENT="Production"
+
+
 # Test creation webapp
 open -a "Google Chrome" http://webtest01.azurewebsites.net
 
@@ -38,6 +81,10 @@ open -a "Google Chrome" http://webtest01.azurewebsites.net
 git submodule add \
     git@github.com:giulianolatini/dotnetWebapp.git \
     dotnetWebapp
+
+git submodule add \
+    https://github.com/azure-samples/dotnetcore-sqldb-tutorial \
+    dotnetcore-sqldb-tutorial
 
 # Connect Webapp Repo to Azure Webapp for deploy
 git remote add \
@@ -52,16 +99,7 @@ az group deployment create \
     --resource-group SandboxGroup02 \
     --template-file Templates/AzureDeploy.json
 
-# Deploy VM
-az group deployment create \
-    --resource-group SandboxGroup02 \
-    --template-file Templates/RHEL/azuredeploy.json \
-    --parameters @Templates/RHEL/azuredeploy.parameters.json \
-    | jq --raw-output '.properties.outputs.sshCommand.value'
-
-# Validation Tests
-
-
 # Cleaner
 az group delete --name SandboxGroup02
 
+Get-AzureRmResourceGroup -Name "SandboxGroup02" | Remove-AzureRmResourceGroup -Verbose -Force
